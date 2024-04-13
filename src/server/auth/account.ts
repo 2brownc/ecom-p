ea54502fa92d009f8fd3405dab5b1f49bc042835
brown <86018533+2brownc@users.noreply.github.com>
2024-04-14 03:17:43 +0530

    utils to handle accounts

---
+++ src/server/auth/account.ts
@@ -0,0 +1,41 @@
+import { sha512, generateRandomNumber } from "./utils";
+import { db } from "../db";
+
+export async function createAccount(
+  name: string,
+  email: string,
+  password: string,
+) {
+  const passhash = sha512(password);
+
+  const result = await db.user.create({
+    data: {
+      name,
+      email,
+      passhash,
+    },
+  });
+
+  return result;
+}
+
+export async function checkUser(email: string, passhash: string) {
+  const user = await db.user.findFirst({
+    where: {
+      AND: [{ email }, { passhash }],
+    },
+  });
+
+  if (!user) {
+    throw new Error("Invalid email or password");
+  }
+
+  return user;
+}
+
+export function getEmailVerificationCodeHash() {
+  const verificationCode = generateRandomNumber();
+  const verificationCodeHash = sha512(verificationCode.toString());
+
+  return verificationCodeHash;
+}
